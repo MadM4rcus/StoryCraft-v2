baf8edebb002ae2ec056b194e80802e7f982d693
MadM4rcus <135142278+MadM4rcus@users.noreply.github.com>
2025-09-12 18:55:48 +0000

    config inicial

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">StoryCraft V2 - Em Construção!</h1>
+    </div>
+  )
+}
+
+export default App;
